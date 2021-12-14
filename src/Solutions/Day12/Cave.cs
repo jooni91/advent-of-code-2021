1c45f47c2a7f4d47d1cca8786e425b179540b682
Joonas Griesinger <joonas.griesinger@jgdevlabs.com>
2021-12-14 17:02:42 +0200

    Save some work on day 12.

---
+++ src/Solutions/Day12/Cave.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2021.Solutions.Day12
+{
+    public class Cave : IEquatable<Cave?>
+    {
+        private readonly List<Cave> _connectedCaves = new List<Cave>();
+        private readonly string _caveId;
+
+        public Cave(string id)
+        {
+            _caveId = id;
+        }
+
+        public bool IsSmallCave => char.IsLower(_caveId[0]);
+
+        public void AddConnection(Cave cave)
+        {
+            _connectedCaves.Add(cave);
+        }
+
+        public bool Equals(Cave? other)
+        {
+            return other != null &&
+                   _caveId == other._caveId;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Cave);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_caveId);
+        }
+        public override string ToString()
+        {
+            return _caveId;
+        }
+
+        public static bool operator ==(Cave? left, Cave? right)
+        {
+            return EqualityComparer<Cave>.Default.Equals(left, right);
+        }
+        public static bool operator !=(Cave? left, Cave? right)
+        {
+            return !(left == right);
+        }
+    }
+}
--- src/Solutions/Day12/Day12.cs
+++ src/Solutions/Day12/Day12.cs
@@ -1,5 +1,6 @@
 ﻿using AdventOfCode2021.Utilities;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2021.Solutions.Day12
@@ -10,12 +11,49 @@ namespace AdventOfCode2021.Solutions.Day12
 
         protected override string PartOne(string input)
         {
+            var caveMap = ParseInput(input);
+
             return "";
         }
 
         protected override string PartTwo(string input)
         {
             return "";
+        }
+
+        private HashSet<Cave> ParseInput(string input)
+        {
+            var caves = new HashSet<Cave>();
+
+            foreach (var line in input.ReadInputLines())
+            {
+                var caveIds = line.Split('-');
+                var cave1 = new Cave(caveIds[0]);
+                var cave2 = new Cave(caveIds[1]);
+
+                if (caves.Contains(cave1))
+                {
+                    cave1 = caves.First(c => c == cave1);
+                }
+                else
+                {
+                    caves.Add(cave1);
+                }
+
+                if (caves.Contains(cave2))
+                {
+                    cave2 = caves.First(c => c == cave2);
+                }
+                else
+                {
+                    caves.Add(cave2);
+                }
+
+                cave1.AddConnection(cave2);
+                cave2.AddConnection(cave1);
+            }
+
+            return caves;
         }
     }
 }
