5b4c68b8397e2e427babb0af125d5e1e7e1c0b43
Joonas Griesinger <joonas.griesinger@jgdevlabs.com>
2021-12-17 15:34:38 +0200

    Added day 16 files.

---
+++ src/Solutions/Day16/Day16.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2021.Utilities;
+using System;
+using System.Linq;
+
+namespace AdventOfCode2021.Solutions.Day16
+{
+    public class Day16 : DayBase
+    {
+        protected override string Day => "16";
+
+        protected override string PartOne(string input)
+        {
+            return "";
+        }
+
+        protected override string PartTwo(string input)
+        {
+            return "";
+        }
+    }
+}
---
+++ tests/Solutions/Day16/Day16Tests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace AdventOfCode2021.Tests.Solutions.Day16
+{
+    public class Day16Tests
+    {
+        //[Fact]
+        public void PartOne_ShouldReturn_ExpectedValue()
+        {
+            // Arrange
+            var daySolution = new AdventOfCode2021.Solutions.Day16.Day16();
+
+            // Act
+            var result = daySolution.GetResult(Part.One);
+
+            // Assert
+            Assert.Equal("", result);
+        }
+
+        //[Fact]
+        public void PartTwo_ShouldReturn_ExpectedValue()
+        {
+            // Arrange
+            var daySolution = new AdventOfCode2021.Solutions.Day16.Day16();
+
+            // Act
+            var result = daySolution.GetResult(Part.Two);
+
+            // Assert
+            Assert.Equal("", result);
+        }
+    }
+}
